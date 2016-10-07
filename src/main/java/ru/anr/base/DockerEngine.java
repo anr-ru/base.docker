da94bb95dcb883969f64659153dce821a64b4e23
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2016-10-07 10:42:53 +0500

    ??

--- src/main/java/ru/anr/base/DockerEngine.java
+++ src/main/java/ru/anr/base/DockerEngine.java
@@ -76,7 +76,6 @@ public class DockerEngine extends BaseParent {
         logger.info("Using DOCKER URL: {}", value);
 
         docker = DockerClientBuilder.getInstance(value).build();
-
     }
 
     /**
