ce03947366340eca8f97a97560bfb9df63e207b1
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2021-03-21 23:36:57 +0500

    MIgrate to Base.Platform 2.0 libraries. Refactoring due to changes

--- src/main/java/ru/anr/base/DockerEngine.java
+++ src/main/java/ru/anr/base/DockerEngine.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2015 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,41 +15,29 @@
  */
 package ru.anr.base;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.util.Map;
-
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.command.*;
+import com.github.dockerjava.api.model.*;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.command.ExecStartResultCallback;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.CreateContainerCmd;
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.command.ExecCreateCmdResponse;
-import com.github.dockerjava.api.command.PullImageCmd;
-import com.github.dockerjava.api.command.PushImageCmd;
-import com.github.dockerjava.api.model.AuthConfig;
-import com.github.dockerjava.api.model.BuildResponseItem;
-import com.github.dockerjava.api.model.Container;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.Identifier;
-import com.github.dockerjava.api.model.Ports;
-import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.core.command.BuildImageResultCallback;
-import com.github.dockerjava.core.command.ExecStartResultCallback;
-import com.github.dockerjava.core.command.PullImageResultCallback;
-import com.github.dockerjava.core.command.PushImageResultCallback;
-import com.github.dockerjava.core.command.WaitContainerResultCallback;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * The Engine is a wrapper around Docker interfaces for mostly used cases.
  *
- *
  * @author Alexey Romanchuk
  * @created May 19, 2015
- *
  */
 public class DockerEngine extends BaseParent {
 
@@ -74,29 +62,24 @@ public class DockerEngine extends BaseParent {
     /**
      * This constructor creates a client instance in case if the docker host
      * differs from the default.
-     * 
-     * @param dockerUrl
-     *            The Docker Url (e.g.tcp://docker:2375)
+     *
+     * @param dockerUrl The Docker Url (e.g.tcp://docker:2375)
      */
     public DockerEngine(String dockerUrl) {
 
         super();
-        /*
-         * For MacOS X and Linux the way to communicate may differ
-         */
-        // String value = System.getProperty("docker.io.url",
-        // "unix:///var/run/docker.sock");
-        // logger.info("Using DOCKER URL: {}", value);
-
-        docker = ((isEmpty(dockerUrl)) ? //
-                DockerClientBuilder.getInstance() : DockerClientBuilder.getInstance(dockerUrl)).build();
+        docker = (isEmpty(dockerUrl) ?
+                DockerClientBuilder.getInstance() :
+                DockerClientBuilder.getInstance(DefaultDockerClientConfig.createDefaultConfigBuilder()
+                        .withDockerHost(dockerUrl)
+                        .build()))
+                .build();
     }
 
     /**
      * Stops the container specified by identifier ignoring any errors
-     * 
-     * @param containerId
-     *            The container id
+     *
+     * @param containerId The container id
      */
     public void stop(String containerId) {
 
@@ -108,41 +91,30 @@ public class DockerEngine extends BaseParent {
 
     /**
      * Removes the container specified by identifier ignoring any errors
-     * 
-     * @param containerId
-     *            The container id
+     *
+     * @param containerId The container id
      */
     public void remove(String containerId) {
-
-        runIgnored(x -> {
-            docker.removeContainerCmd(containerId).exec();
-        });
+        runIgnored(x -> docker.removeContainerCmd(containerId).exec());
     }
 
     /**
      * Removes (forcedly) the specified image
-     * 
-     * @param imageId
-     *            The identifier of the image
+     *
+     * @param imageId The identifier of the image
      */
     public void removeImage(String imageId) {
 
         logger.info("Removing the image: {}", imageId);
-
-        runIgnored(x -> {
-            docker.removeImageCmd(imageId).withForce(true).exec();
-        });
+        runIgnored(x -> docker.removeImageCmd(imageId).withForce(true).exec());
     }
 
     /**
      * Builds an image by specified parameters
-     * 
-     * @param directory
-     *            A directory with "Dockerfile"
-     * @param repository
-     *            A repository to use
-     * @param tag
-     *            A tag to use
+     *
+     * @param directory  A directory with "Dockerfile"
+     * @param repository A repository to use
+     * @param tag        A tag to use
      */
     public void build(File directory, String repository, String tag) {
 
@@ -150,7 +122,6 @@ public class DockerEngine extends BaseParent {
 
             @Override
             public void onNext(BuildResponseItem item) {
-
                 logger.debug("Build: {}", item.getStream());
                 super.onNext(item);
             }
@@ -161,35 +132,32 @@ public class DockerEngine extends BaseParent {
 
     /**
      * Pulls the given image from the specified repository
-     * 
-     * @param repository
-     *            The repository (the full name)
-     * @param cfg
-     *            The authenticate configuration
+     *
+     * @param repository The repository (the full name)
+     * @param cfg        The authenticate configuration
      */
     public void pull(String repository, AuthConfig cfg) {
 
         Identifier identifier = Identifier.fromCompoundString(repository);
 
         PullImageCmd pull = docker.pullImageCmd(identifier.repository.name);
-        if (identifier.tag.isPresent()) {
-            pull.withTag(identifier.tag.get());
-        }
+        identifier.tag.ifPresent(pull::withTag);
         if (cfg != null) {
             pull.withAuthConfig(cfg);
         }
-        pull.exec(new PullImageResultCallback()).awaitSuccess();
+        try {
+            pull.exec(new PullImageResultCallback()).awaitCompletion();
+        } catch (InterruptedException ex) {
+            throw new ApplicationException(ex);
+        }
     }
 
     /**
      * Pushing the image given by separate parts
-     * 
-     * @param repository
-     *            The name of a repository
-     * @param tag
-     *            A tag
-     * @param cfg
-     *            The configuration for accessing to repositories
+     *
+     * @param repository The name of a repository
+     * @param tag        A tag
+     * @param cfg        The configuration for accessing to repositories
      */
     public void push(String repository, String tag, AuthConfig cfg) {
 
@@ -199,18 +167,20 @@ public class DockerEngine extends BaseParent {
         if (cfg != null) {
             push.withAuthConfig(cfg);
         }
-        push.exec(new PushImageResultCallback()).awaitSuccess();
+        try {
+            push.exec(new ResultCallback.Adapter<>()).awaitCompletion();
+        } catch (InterruptedException ex) {
+            throw new ApplicationException(ex);
+        }
     }
 
     /**
      * Executes the command line specified in arguments in a container
-     * 
+     * <p>
      * (like 'cat /tmp/txt.txt')
-     * 
-     * @param containerId
-     *            The identifier of a container
-     * @param cmds
-     *            List of commands to execute inside of the container
+     *
+     * @param containerId The identifier of a container
+     * @param cmds        List of commands to execute inside of the container
      * @return The command output
      */
     public String exec(String containerId, String... cmds) {
@@ -222,7 +192,9 @@ public class DockerEngine extends BaseParent {
 
         try {
 
-            docker.execStartCmd(containerId).withExecId(rs.getId()).exec(new ExecStartResultCallback(out, out))
+            docker.execStartCmd(containerId)
+                    .withExecId(rs.getId())
+                    .exec(new ExecStartResultCallback(out, out))
                     .awaitCompletion();
 
             String s = out.toString(DEFAULT_CHARSET.name());
@@ -235,25 +207,21 @@ public class DockerEngine extends BaseParent {
 
     /**
      * Creates and runs a container by the given image
-     * 
-     * @param image
-     *            The image to use
-     * @param name
-     *            The name to use for the new container
-     * @param cmd
-     *            The command to use
-     * @param envs
-     *            The en
-     * @param portPairs
-     *            The pairs with bindings (host,container,host,container)
+     *
+     * @param image     The image to use
+     * @param name      The name to use for the new container
+     * @param cmd       The command to use
+     * @param envs      The en
+     * @param portPairs The pairs with bindings (host,container,host,container)
      * @return The identifier of the container
      */
     public String start(String image, String name, String cmd, String[] envs, Integer... portPairs) {
 
         final Ports bindings = getBindings(portPairs);
         return start(image, name, c -> {
-
-            c.withCmd(StringUtils.split(cmd, " ")).withPortBindings(bindings);
+            Objects.requireNonNull(c.withCmd(StringUtils.split(cmd, " ")).
+                    getHostConfig())
+                    .withPortBindings(bindings);
             if (envs != null) {
                 c.withEnv(envs);
             }
@@ -263,9 +231,8 @@ public class DockerEngine extends BaseParent {
     /**
      * Converts a plain port binding to the special structure suitable for
      * processing by the engine.
-     * 
-     * @param portPairs
-     *            The pairs (host,container) for the required ports
+     *
+     * @param portPairs The pairs (host,container) for the required ports
      * @return A {@link Ports} object instance
      */
     public static Ports getBindings(Integer... portPairs) {
@@ -273,29 +240,24 @@ public class DockerEngine extends BaseParent {
         Map<Integer, Integer> map = toMap(portPairs);
         final Ports bindings = new Ports();
 
-        map.forEach((h, c) -> {
-            bindings.bind(ExposedPort.tcp(c), Ports.Binding.bindPort(h));
-        });
+        map.forEach((h, c) -> bindings.bind(ExposedPort.tcp(c), Ports.Binding.bindPort(h)));
         return bindings;
     }
 
     /**
      * Creates and runs a container with the manual way to configure its
      * parameters
-     * 
-     * @param image
-     *            The image to use
-     * @param name
-     *            The name to use for the new container
-     * @param callback
-     *            A special callback to get {@link CreateContainerCmd} for
-     *            further configuration.
+     *
+     * @param image    The image to use
+     * @param name     The name to use for the new container
+     * @param callback A special callback to get {@link CreateContainerCmd} for
+     *                 further configuration.
      * @return The identifier of new created container
      */
-    public String start(String image, String name, DockerRunConfigurator callback) {
+    public String start(String image, String name, Consumer<CreateContainerCmd> callback) {
 
         CreateContainerCmd cmd = docker.createContainerCmd(image).withName(name);
-        callback.configure(cmd);
+        callback.accept(cmd);
 
         CreateContainerResponse rs = cmd.exec();
 
@@ -307,7 +269,7 @@ public class DockerEngine extends BaseParent {
 
     /**
      * Returns all active containers
-     * 
+     *
      * @return A map which has the identifiers of containers as the keys
      */
     public Map<String, Container> getActive() {
@@ -317,13 +279,10 @@ public class DockerEngine extends BaseParent {
 
     /**
      * Committing the container to fix and store all changes as a new image
-     * 
-     * @param containerId
-     *            The identifier of the container
-     * @param repository
-     *            The name of a repository to use
-     * @param tag
-     *            The tag to use
+     *
+     * @param containerId The identifier of the container
+     * @param repository  The name of a repository to use
+     * @param tag         The tag to use
      * @return The identifier of the newly created image
      */
     public String commit(String containerId, String repository, String tag) {
